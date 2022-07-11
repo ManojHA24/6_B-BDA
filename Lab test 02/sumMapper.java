7f468081d623b1b7acc2e974c446851c59c5ec44
ManojHA24 <71307033+ManojHA24@users.noreply.github.com>
2022-07-11 15:58:28 +0530

    Create sumMapper.java

---
+++ Lab test 02/sumMapper.java
@@ -0,0 +1,36 @@
+package sumofsquare;
+
+//Importing libraries
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class sumMapper extends MapReduceBase implements Mapper<LongWritable,
+Text, Text, IntWritable> {
+
+@Override
+// Map function
+public void map(LongWritable key, Text value, OutputCollector<Text,
+IntWritable> output, Reporter rep)
+
+throws IOException
+{
+// Splitting the line into spaces
+String data[] = value.toString().split(" ");
+
+for (String num : data)
+{
+
+int number = Integer.parseInt(num);
+
+output.collect(new Text("square"), new IntWritable(number));
+
+
+}
+}
+}
